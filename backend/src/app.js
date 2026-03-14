c64d1be257ec0c015033aded2da5b966787104d9
Meghana <meghanaponna234@gmail.com>
2026-03-14 19:14:17 +0530

    fixed cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,64 @@
+// // import express from "express";
+// // import cors from "cors";  
+// // import User from "./models/User.js";
+// // import authRoutes from "./routes/auth.routes.js";
+// // import userRoutes from "./routes/user.routes.js";
+
+
+
+// // const app = express();
+
+// // app.use(express.json());
+// // /* 🔥 CORS CONFIG */
+// // app.use(
+// //   cors({
+// //     origin: "https://2-factor-authentication-lovat.vercel.app", // frontend
+// //     credentials: true,
+// //   })
+// // );
+
+// // app.get("/", (req, res) => {
+// //   res.status(200).send("Backend is running");
+// // });
+
+// // app.use("/api/auth", authRoutes);
+// // app.use("/api/user", userRoutes);
+
+// // export default app;
+
+
+
 // import express from "express";
-// import cors from "cors";  
+// import cors from "cors";
 // import User from "./models/User.js";
 // import authRoutes from "./routes/auth.routes.js";
 // import userRoutes from "./routes/user.routes.js";
 
-
-
 // const app = express();
 
 // app.use(express.json());
+
 // /* 🔥 CORS CONFIG */
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://2-factor-authentication-lovat.vercel.app"
+// ];
+
 // app.use(
 //   cors({
-//     origin: "https://2-factor-authentication-lovat.vercel.app", // frontend
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("CORS not allowed"));
+//       }
+//     },
 //     credentials: true,
 //   })
 // );
+
+// /* Handle preflight requests */
+// app.options("*", cors());
 
 // app.get("/", (req, res) => {
 //   res.status(200).send("Backend is running");
@@ -28,9 +71,9 @@
 
 
 
+
 import express from "express";
 import cors from "cors";
-import User from "./models/User.js";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
@@ -38,7 +81,7 @@ const app = express();
 
 app.use(express.json());
 
-/* 🔥 CORS CONFIG */
+/* CORS CONFIG */
 const allowedOrigins = [
   "http://localhost:5173",
   "https://2-factor-authentication-lovat.vercel.app"
@@ -46,19 +89,10 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("CORS not allowed"));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
   })
 );
-
-/* Handle preflight requests */
-app.options("*", cors());
 
 app.get("/", (req, res) => {
   res.status(200).send("Backend is running");
