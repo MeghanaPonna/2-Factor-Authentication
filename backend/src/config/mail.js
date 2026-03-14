5ac4c98f04f066c06fd694749d2842a835fd5e4c
Meghana <meghanaponna234@gmail.com>
2026-03-14 19:07:28 +0530

    fix nodemailer smtp port

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,10 +1,29 @@
+// import nodemailer from "nodemailer";
+// import dotenv from "dotenv";
+
+// dotenv.config(); // 🔥 THIS IS THE FIX
+
+// const transporter = nodemailer.createTransport({
+//   service: "Gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// export default transporter;
+
+
+
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
-dotenv.config(); // 🔥 THIS IS THE FIX
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // important for port 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
